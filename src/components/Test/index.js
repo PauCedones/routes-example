9c81507e890094958b442e96dd13b4c42aae2842
Paula <cedones@gmail.com>
2021-03-04 23:39:25 -0300

    agregando scss

---
+++ src/components/Test/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.scss";
+
+function Test() {
+  return (
+    <div>
+      <p className="primerTest">soy un test</p>
+      <p> soy otro test</p>
+    </div>
+  );
+}
+
+export default Test;
